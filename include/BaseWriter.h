b855e5e70865b64d57aec206459f377721d44af2
godka <asdic182@sina.com>
2016-04-11 19:12:51 +0800

    格式搞对了再说

--- include/BaseWriter.h
+++ include/BaseWriter.h
@@ -3,25 +3,27 @@
 #include <ostream>
 #include "FlvCommon.h"
 
-namespace BrightLib{namespace Media{
+namespace BrightLib{
+	namespace Media{
 
-	class LIBFLV_API BaseWriter
-	{
-	public:
-		virtual int write(uint8_t *buf, size_t len)=0;
-	};
-	class FileWriter
-	{
-	};
-	class LIBFLV_API StreamWriter : public BaseWriter
-	{
-	private:
-		std::ostream& m_output;
-	public:
-		StreamWriter(std::ostream& st);
-		virtual int write(uint8_t *buf, size_t len);
-	};
+		class LIBFLV_API BaseWriter
+		{
+		public:
+			virtual int write(uint8_t *buf, size_t len) = 0;
+		};
+		class FileWriter
+		{
+		};
+		class LIBFLV_API StreamWriter : public BaseWriter
+		{
+		private:
+			std::ostream& m_output;
+		public:
+			StreamWriter(std::ostream& st);
+			virtual int write(uint8_t *buf, size_t len);
+		};
 
-}}
+	}
+}
 
 #endif //_BASE_WRITER_H
--- src/BaseWriter.cpp
+++ src/BaseWriter.cpp
@@ -1,16 +1,18 @@
 #include "BaseWriter.h"
 
-namespace BrightLib{namespace Media{
+namespace BrightLib{
+	namespace Media{
 
-	StreamWriter::StreamWriter(std::ostream& st):m_output(st)
-	{
+		StreamWriter::StreamWriter(std::ostream& st) :m_output(st)
+		{
+
+		}
+
+		int StreamWriter::write(uint8_t *buf, size_t len)
+		{
+			m_output.write((char*) buf, len);
+			return 0;
+		}
 
 	}
-
-	int StreamWriter::write(uint8_t *buf, size_t len)
-	{
-		m_output.write((char*)buf, len);
-		return 0;
-	}
-
-}}
+}
